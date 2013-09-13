96498a337daa07bdf8f6f10e990f65198a1da57a
Malitha Shan <msp7956@gmail.com>
2013-09-13 15:23:48 +0530

    Login Form

---
+++ MobileShoppingSystem/src/com/example/mobileshoppingsystem/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mobileshoppingsystem;
+
+import org.apache.cordova.*;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends  DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+	
+}
