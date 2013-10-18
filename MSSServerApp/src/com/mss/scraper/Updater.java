5242a3ea18b689aa0bea572c4e93ebb5298c1aeb
prabhath5510 <prabhath5510polo@gmail.com>
2013-10-18 08:56:27 +0530

    scraper class added

--- MSSServerApp/src/com/mss/scraper/Updater.java
+++ MSSServerApp/src/com/mss/scraper/Updater.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mss.scraper_dbupdate;
+package com.mss.scraper;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
